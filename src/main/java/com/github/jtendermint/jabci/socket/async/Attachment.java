cc9963660e1f6699a70585a173b78f64b1f89780
Wolf Posdorfer <posdorfer@informatik.uni-hamburg.de>
2017-03-14 16:53:58 +0100

    final formatting

--- src/main/java/com/github/jtendermint/jabci/socket/async/Attachment.java
+++ src/main/java/com/github/jtendermint/jabci/socket/async/Attachment.java
@@ -1,7 +1,28 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2016 - 2017
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.github.jtendermint.jabci.socket.async;
 
-import java.io.IOException;
-import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
@@ -11,7 +32,6 @@ public class Attachment {
     public final AsynchronousServerSocketChannel server;
     public final AsynchronousSocketChannel client;
     public ByteBuffer buffer;
-    public SocketAddress clientAddress;
     public boolean isRead;
 
     public Attachment(AsynchronousServerSocketChannel server, AsynchronousSocketChannel client, ByteBuffer buffer, boolean isRead) {
@@ -19,14 +39,6 @@ public class Attachment {
         this.server = server;
         this.client = client;
         this.buffer = buffer;
-        try {
-            if (client != null) {
-                this.clientAddress = client.getRemoteAddress();
-            }
-        } catch (IOException e) {
-            this.clientAddress = null;
-            e.printStackTrace();
-        }
         this.isRead = isRead;
 
     }
@@ -35,8 +47,7 @@ public class Attachment {
     public boolean equals(Object obj) {
         if (obj instanceof Attachment) {
             Attachment other = (Attachment) obj;
-            return Objects.equals(server, other.server) && Objects.equals(client, other.client)
-                    && Objects.equals(clientAddress, other.clientAddress);
+            return Objects.equals(server, other.server) && Objects.equals(client, other.client);
         }
         return false;
     }
@@ -55,10 +66,9 @@ public class Attachment {
 
         try {
             return "Attachment [server=" + server.getLocalAddress() + ", client=" + client.getLocalAddress() + "/"
-                    + client.getRemoteAddress() + ", buffer=" + buffer + ", clientAddress=" + clientAddress + ", isRead=" + isRead + "]";
+                    + client.getRemoteAddress() + ", buffer=" + buffer + ", isRead=" + isRead + "]";
         } catch (Exception e) {
-            return "Attachment [server=" + server + ", client=" + client + ", buffer=" + buffer + ", clientAddress=" + clientAddress
-                    + ", isRead=" + isRead + "]";
+            return "Attachment [server=" + server + ", client=" + client + ", buffer=" + buffer + ", isRead=" + isRead + "]";
         }
     }
 
